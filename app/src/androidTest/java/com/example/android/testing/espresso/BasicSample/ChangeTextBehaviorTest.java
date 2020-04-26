1430847acb210485150f520adb152fe9e307d26a
moatazeldebsy <moataznabil2019@gmail.com>
2020-04-26 12:33:10 +0200

    add the Basic example

---
+++ app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/ChangeTextBehaviorTest.java
@@ -0,0 +1,38 @@
+package com.example.android.testing.espresso.BasicSample;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ChangeTextBehaviorTest {
+
+    private static final String messageText = "Espresso";
+
+    @Rule public ActivityTestRule<MainActivity> activityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void changeText_sameActivity() {
+        onView(withId(R.id.editTextUserInput))
+                .perform(typeText(messageText), closeSoftKeyboard());
+
+        onView(withId(R.id.changeTextBt))
+            .perform(click());
+
+        onView(withId(R.id.textToBeChanged))
+            .check(matches(withText(messageText)));
+    }
+}
