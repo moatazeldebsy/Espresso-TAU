25068a00fc03897e21b7d9514946ec849d2f290d
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-04-29 18:59:22 +0200

    Update WebViewActivityTest.java

--- app/src/androidTest/java/com/example/android/testing/espresso/web/BasicSample/WebViewActivityTest.java
+++ app/src/androidTest/java/com/example/android/testing/espresso/web/BasicSample/WebViewActivityTest.java
@@ -1,16 +1,16 @@
 package com.example.android.testing.espresso.web.BasicSample;
 
+
+import android.content.Intent;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import android.content.Intent;
-
 import androidx.test.espresso.web.webdriver.DriverAtoms;
 import androidx.test.espresso.web.webdriver.Locator;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
@@ -18,35 +18,44 @@ import static androidx.test.espresso.web.webdriver.DriverAtoms.clearElement;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class WebViewActivityTest {
 
-    private static final String messageText = "TAU";
-
     @Rule
-    public ActivityTestRule<WebViewActivity> mActivityRule = new ActivityTestRule<WebViewActivity>(
-            WebViewActivity.class, false, false) {
-        @Override
-        protected void afterActivityLaunched() {
-            onWebView().forceJavascriptEnabled();
-        }
-    };
+    public ActivityTestRule<WebViewActivity> mActivity =
+        new ActivityTestRule<WebViewActivity>
+            (WebViewActivity.class,false,false)
+        {
+            @Override
+            protected void afterActivityLaunched() {
+                onWebView().forceJavascriptEnabled();
+            }
+        };
 
     @Test
-    public void typeTextInInput_clickButton_SubmitsForm() {
+    public void typeText_SubmitForm()
+    {
         Intent webFormIntent = new Intent();
-        mActivityRule.launchActivity(webFormIntent);
+        mActivity.launchActivity(webFormIntent);
+
+        //text_input
+        //submitBtn
+        //response
+
         onWebView()
-            .withElement(findElement(Locator.ID, "text_input"))
+            .withElement(findElement(Locator.ID,"text_input"))
             .perform(clearElement())
-            .perform(DriverAtoms.webKeys(messageText))
+            .perform(DriverAtoms.webKeys("TAU"));
 
-            .withElement(findElement(Locator.ID, "submitBtn"))
-            .perform(webClick())
+        onWebView()
+            .withElement(findElement(Locator.ID,"submitBtn"))
+            .perform(webClick());
 
-            .withElement(findElement(Locator.ID, "response"))
-            .check(webMatches(getText(), containsString(messageText)));
+        onWebView()
+            .withElement(findElement(Locator.ID,"response"))
+            .check(webMatches(getText(),containsString("TAU")));
     }
+
 }
