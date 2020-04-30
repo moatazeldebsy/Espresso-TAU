b27b057cc6417865acc3a4f3a6d68c991c658ea8
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-04-30 12:15:32 +0200

    Update CucumberTestRunner.java

--- app/src/androidTest/java/com/sniper/bdd/cucumber/runner/CucumberTestRunner.java
+++ app/src/androidTest/java/com/sniper/bdd/cucumber/runner/CucumberTestRunner.java
@@ -3,28 +3,18 @@ package com.sniper.bdd.cucumber.runner;
 import android.os.Bundle;
 import com.sniper.bdd.BuildConfig;
 
-import androidx.test.runner.AndroidJUnitRunner;
-import cucumber.api.android.CucumberInstrumentationCore;
+import cucumber.api.android.CucumberAndroidJUnitRunner;
 
 @SuppressWarnings("unused")
-public class CucumberTestRunner extends AndroidJUnitRunner {
+public class CucumberTestRunner extends CucumberAndroidJUnitRunner {
 
     private static final String CUCUMBER_TAGS_KEY = "tags";
     private static final String CUCUMBER_SCENARIO_KEY = "name";
-    private final CucumberInstrumentationCore instrumentationCore =
-        new CucumberInstrumentationCore(this);
 
     @Override
     public void onCreate(final Bundle bundle) {
         String tags = BuildConfig.TEST_TAGS;
         String scenario = BuildConfig.TEST_SCENARIO;
-        instrumentationCore.create(bundle);
         super.onCreate(bundle);
-    }
-
-    @Override
-    public void onStart() {
-        waitForIdleSync();
-       instrumentationCore.start();
     }
 }
